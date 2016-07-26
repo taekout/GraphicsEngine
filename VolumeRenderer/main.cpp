2981c4c5435af51f78c5f81a67f79d3b00a1c787
Taekyu <taekout@gmail.com>
2016-07-25 23:30:22 -0400

    GFW Initial

--- VolumeRenderer/main.cpp
+++ VolumeRenderer/main.cpp
@@ -489,9 +489,42 @@ void LoadSkybox()
 int main(int argc, char **argv) {
 	try {
 
+		if (!glfwInit()) {
+			return -1;
+		}
+
+		glfwWindowHint(GLFW_SAMPLES, 4); // 4x antialiasing
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // We want OpenGL 3.3
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
+		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); //We don't want the old OpenGL 
+
+		GLFWwindow* window = glfwCreateWindow(1024, 768, "Tutorial 01", NULL, NULL);
+		if (window == NULL) {
+			fprintf(stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n");
+			getchar();
+			glfwTerminate();
+			return -1;
+		}
+		glfwMakeContextCurrent(window);
+
+		// Initialize GLEW
+		if (glewInit() != GLEW_OK) {
+			fprintf(stderr, "Failed to initialize GLEW\n");
+			getchar();
+			glfwTerminate();
+			return -1;
+		}
+
+		// Ensure we can capture the escape key being pressed below
+		glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+		// Dark blue background
+		glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+#if 0
 		glutInit(&argc, argv);
 		InitGL();
-		if (glewIsSupported("GL_VERSION_3_1"))
+		if (glewIsSupported("GL_VERSION_3_3"))
 			printf("Ready for OpenGL 3.1.\n");
 		else {
 			printf("OpenGL 3.1 not supported\n");
@@ -591,7 +624,7 @@ int main(int argc, char **argv) {
 		glutMainLoop();
 
 		EndGL();
-
+#endif
 	}
 	catch (char * e) {
 		std::cout << e << std::endl;
